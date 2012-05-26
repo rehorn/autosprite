51aeea2cf4d06c98b2517369a422a148637ea429
rehorn <sysucgh@163.com>
2012-05-26 14:17:54 +0800

    packer replace ready

--- autosprite.py
+++ autosprite.py
@@ -2,26 +2,37 @@
 #encoding=utf-8
 #python2.6
 
-import sys, os, re
+import sys, os, re, copy
 from PIL import Image as PImage
 
 # default setting
 CONFIG = {
-    'VERSION': '0.0.1',
+    'VERSION': '0.1.1',
     'TRANSPARENT': (255, 255, 255, 0),
     'IS_QUIET': False,
+    'INDENT': 4,
     'ROOT_SPRITE_NAME': 'app-sprite',
     'ALLOW_IMG_EXT': ['png', 'jpg', 'jpeg', 'gif'],
     'ALLOW_CSS_EXT': ['css'],
-    'PACKER': 'vertical',
+    # 'PACKER': 'horizontal',
+    'PACKER': 'packed',
     'REPLACER': 'simple',
+    'IMG_ORDER': 'maxside',
+    'CSS_INPUT': './css/',
+    'IMG_INPUT': './img/',
+    # 'IMG_INPUT': './last-guardian-sprites/',
     'CSS_OUTPUT': './',
-    'SPRITE_OUTPUT': './',
+    'SPRITE_OUTPUT': './images2/',
     'IMAGE_URL_REGX': 'url\(("|\')?(.*?)("|\')?\)'
 }
 
 # cache all images
 CACHE = {}
+ORDERINGS = ['maxside', 'width', 'height', 'area']
+
+# alias
+opath = os.path
+
 
 def log(msg, spliteLine = ''):
     if not CONFIG['IS_QUIET']:
@@ -35,14 +46,29 @@ def checkExt(path, allowExts = CONFIG['ALLOW_IMG_EXT']):
     extRegx = re.compile('.+\.(%s)$' % exts, re.IGNORECASE)
     return not path.startswith('.') and extRegx.match(path)
 
+def mkdir(path):
+    path = opath.dirname(path)
+    if not opath.exists(path):
+        os.makedirs(path)
+
 # error handling
 class PILError(Exception):
 
     """PIL Python package is not installed"""
     err_code = 1
 
+class PackedSortError(Exception):
+
+    """packed packer not sort currectly
+    can't fit block into root - this should not happen if images are pre-sorted correctly
+    """
+    err_code = 11
+
 
 # sprite packer algorithm
+# reference: 
+# sprite-factory: https://github.com/jakesgordon/sprite-factory
+# glue: https://github.com/jorgebastida/glue
 class VerticalPacker(object):
 
     def process(self, sprite):
@@ -52,10 +78,127 @@ class VerticalPacker(object):
             image.y = y
             y += image.absoluteHeight
 
+class HorizontalPacker(object):
+
+    def process(self, sprite):
+        x = 0
+        for image in sprite.images:
+            image.y = 0
+            image.x = x
+            x += image.absoluteWidth
+
+# packed-packer: http://codeincomplete.com/posts/2011/5/7/bin_packing/example/
+# packed-js-ver: http://codeincomplete.com/posts/2011/5/7/bin_packing/growing_packer.js
+class PackedNode(object):
+
+    def __init__(self, x=0, y=0, width=0, height=0, used=False,
+                 down=None, right=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.used = used
+        self.right = right
+        self.down = down
+
+    def find(self, node, width, height):
+        if node.used:
+            return self.find(node.right, width, height) or \
+                   self.find(node.down, width, height)
+        elif node.width >= width and node.height >= height:
+            return node
+        return None
+
+    def grow(self, width, height):
+        can_grow_d = width <= self.width
+        can_grow_r = height <= self.height
+        should_grow_r = can_grow_r and self.height >= (self.width + width)
+        should_grow_d = can_grow_d and self.width >= (self.height + height)
+
+        if should_grow_r:
+            return self.grow_right(width, height)
+        elif should_grow_d:
+            return self.grow_down(width, height)
+        elif can_grow_r:
+            return self.grow_right(width, height)
+        elif can_grow_d:
+            return self.grow_down(width, height)
+        else:
+            raise PackedSortError()
+        return None
+
+    def grow_right(self, width, height):
+        old_self = copy.copy(self)
+        self.used = True
+        self.x = self.y = 0
+        self.width += width
+        self.down = old_self
+        self.right = PackedNode(x = old_self.width,
+                                 y = 0,
+                                 width = width,
+                                 height = self.height)
+
+        node = self.find(self, width, height)
+        if node:
+            return self.split(node, width, height)
+        return None
+
+    def grow_down(self, width, height):
+        old_self = copy.copy(self)
+        self.used = True
+        self.x = self.y = 0
+        self.height += height
+        self.right = old_self
+        self.down = PackedNode(x = 0,
+                                y = old_self.height,
+                                width = self.width,
+                                height = height)
+
+        node = self.find(self, width, height)
+        if node:
+            return self.split(node, width, height)
+        return None
+
+    def split(self, node, width, height):
+        node.used = True
+        node.down = PackedNode(x = node.x,
+                                y = node.y + height,
+                                width = node.width,
+                                height = node.height - height)
+        node.right = PackedNode(x = node.x + width,
+                                 y = node.y,
+                                 width = node.width - width,
+                                 height = height)
+        return node
+
+
+class PackedPacker(object):
+
+    def process(self, sprite):
+        if not len(sprite.images):
+            log('__ no images to packed ___')
+            return
+
+        root = PackedNode(width = sprite.images[0].absoluteWidth,
+                    height = sprite.images[0].absoluteHeight)
+
+        # Loot all over the images creating a binary tree
+        for image in sprite.images:
+            node = root.find(root, image.absoluteWidth, image.absoluteHeight)
+            if node:  # Use this node
+                node = root.split(node, image.absoluteWidth,
+                                        image.absoluteHeight)
+            else:  # Grow the canvas
+                node = root.grow(image.absoluteWidth, image.absoluteHeight)
+
+            image.x = node.x
+            image.y = node.y
 
 # packer mapping
 PACKER = {
-    'vertical': VerticalPacker
+    'vertical': VerticalPacker,
+    'horizontal': HorizontalPacker,
+    'packed': PackedPacker
 }
 
 # css replacer
@@ -64,12 +207,27 @@ class SimpleReplacer(object):
     def process(self, cssFile):
         log('SimpleReplacer')
         imgRegx = re.compile(CONFIG['IMAGE_URL_REGX'], re.IGNORECASE)
+        t = []
         for line in open(cssFile.path).xreadlines():
             imgUrl = imgRegx.search(line)
             if imgUrl:
-                absPath = os.path.abspath(imgUrl.group(2))
-                img = CACHE[absPath]
+                dirName =  opath.dirname(cssFile.path)
+                url = opath.join(dirName, imgUrl.group(2))
+                absPath = opath.abspath(url)
+                img = CACHE.get(absPath)
+                if img:
+                    spriteDir = opath.dirname(img.sprite.output)
+                    cssDir = opath.dirname(cssFile.output)
+                    relPath = opath.relpath(spriteDir, cssDir)
+                    spriteRelPath = opath.join(relPath, img.sprite.fileName)
+                    t.append(imgRegx.sub('url(\"%s\")' % spriteRelPath.replace('\\','/'), line))
+                    t.append('%sbackground-position: -%spx -%spx;\n' % ( ' ' * CONFIG['INDENT'], img.x, img.y))
+            else:
+                t.append(line)
 
+        mkdir(cssFile.output)
+        open(cssFile.output,"w").write("".join(t))
+        log('save css file ' + cssFile.output)
 
 REPLACER = {
     'simple': SimpleReplacer
@@ -79,15 +237,15 @@ REPLACER = {
 class Image(object):
 
     def __init__(self, name, sprite):
-        self.x = None
-        self.y = None
+        self.x = 0
+        self.y = 0
         self.name = name
         self.sprite = sprite
         self.fileName, self.format = name.rsplit('.', 1)
 
-        imagePath = os.path.join(sprite.path, name)
+        imagePath = opath.join(sprite.path, name)
         # imagePath = imagePath.replace('\\', '/')
-        self.absPath = os.path.abspath(imagePath)
+        self.absPath = opath.abspath(imagePath)
         CACHE[self.absPath] = self
         imageFile = open(imagePath, "rb")
 
@@ -113,6 +271,24 @@ class Image(object):
         self.absoluteWidth = self.width
         self.absoluteHeight = self.height
 
+    def __lt__(self, img):
+        ordering = CONFIG['IMG_ORDER']
+        ordering = ordering[1:] if ordering.startswith('-') else ordering
+
+        if ordering not in ORDERINGS:
+            raise InvalidImageOrderingError(ordering)
+
+        if ordering == 'width':
+            return self.absoluteWidth <= img.absoluteWidth
+        elif ordering == 'height':
+            return self.absoluteHeight <= img.absoluteHeight
+        elif ordering == 'area':
+            return self.absoluteWidth * self.absoluteHeight <= \
+                   img.absoluteWidth * img.absoluteHeight
+        else:
+            return max(self.absoluteWidth, self.absoluteHeight) <= \
+                   max(img.absoluteWidth, img.absoluteHeight)
+
 class Sprite(object):
 
     def __init__(self, name, path, manager):
@@ -121,6 +297,7 @@ class Sprite(object):
         self.manager = manager
         self.images = []
         self.path = path
+        self.output = ''
         self.process();
 
     def process(self):
@@ -133,7 +310,11 @@ class Sprite(object):
 
     def save(self):
         log("Creating [%s] image file..." % self.name)
-        outputPath = CONFIG['SPRITE_OUTPUT']
+        outputPath = self.manager.output
+
+        if not len(self.images):
+            log('__ no images to sprite ___')
+            return
 
         # cal sprite canvas width & height
         width = height = 0
@@ -153,9 +334,11 @@ class Sprite(object):
             canvas.paste(image.image, (image.x, image.y))
 
         # Save png
-        spriteName = '%s.png' % self.fileName
-        spritePath = os.path.join(outputPath, spriteName)
+        spriteName = self.fileName
+        spritePath = opath.join(outputPath, spriteName)
+        self.output = spritePath
 
+        mkdir(spritePath)
         args, kwargs = [spritePath], dict(optimize = True)
         canvas.save(*args, **kwargs)
         
@@ -168,11 +351,13 @@ class Sprite(object):
         for image in images:
             log('    images %s ' % image.name)
 
-        return sorted(images)
+        # packed packer algothm is not support reverse ordering
+        pack = CONFIG['PACKER']
+        return sorted(images, reverse = not (pack != 'packed' and CONFIG['IMG_ORDER'][0] != '-'))
 
     @property
     def fileName(self):
-        return self.name
+        return '%s.png' % self.name
 
 class SpriteManager(object):
 
@@ -188,8 +373,8 @@ class SpriteManager(object):
         self.generateSprite(self.path, CONFIG['ROOT_SPRITE_NAME'])
         # sprite images @ sub folders (module)
         for spriteName in os.listdir(self.path):
-            path = os.path.join(self.path, spriteName)
-            if os.path.isdir(path) and not spriteName.startswith('.'):
+            path = opath.join(self.path, spriteName)
+            if opath.isdir(path) and not spriteName.startswith('.'):
                 self.generateSprite(path, spriteName)
 
         self.saveSprite()
@@ -206,8 +391,9 @@ class SpriteManager(object):
 
 class CssFile(object):
 
-    def __init__(self, path):
+    def __init__(self, path, output):
         self.path = path
+        self.output = output
         pass
 
     def process(self):
@@ -229,7 +415,7 @@ class CssManager(object):
         for root, dirs, files in os.walk(self.path, True):
             for f in files:
                 if checkExt(f, CONFIG['ALLOW_CSS_EXT']):
-                    css = CssFile(os.path.join(self.path, f))
+                    css = CssFile(opath.join(self.path, f), opath.join(self.output, f))
                     self.cssFiles.append(css)
 
     def _replaceCss(self):
@@ -244,10 +430,10 @@ class CssManager(object):
 
 # main
 def main():
-    imgSource = './images/'
-    imgOutput = None
-    cssSource = './css/'
-    cssOutput = None
+    imgSource = CONFIG['IMG_INPUT']
+    imgOutput = CONFIG['SPRITE_OUTPUT']
+    cssSource = CONFIG['CSS_INPUT']
+    cssOutput = CONFIG['CSS_OUTPUT']
     spriteMan = SpriteManager(imgSource, imgOutput)
     spriteMan.process()
     cssMan = CssManager(cssSource, cssOutput)
